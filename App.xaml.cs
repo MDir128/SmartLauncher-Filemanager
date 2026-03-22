52c7b7a4f9f1dbb5f673ccba11152519401d3787
MDir <zverev_andrey06@mail.ru>
2026-03-22 14:46:27 +0300

    initial

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SmartLauncher_Filemanager
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
